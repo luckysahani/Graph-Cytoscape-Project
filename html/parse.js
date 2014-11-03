c1d0c5df26fc6725eeaca2d940385387ec9ddd4f
hardikbansal <hardikbansal24@gmail.com>
2014-11-03 14:32:30 +0530

    test neo4j

---
+++ html/parse.js
@@ -0,0 +1,15 @@
+function alert1(){
+	var str = document.getElementById('graph_input');
+	var split = str.value.split('\n');
+	var len = split.length;
+	for (var i = 0; i < len; i++){
+		alert(split[i]);
+	}
+	var r=require("request");
+	var txUrl = "http://localhost:7474/db/data/transaction/commit";
+	function cypher(query,params,cb) {
+	  r.post({uri:txUrl,
+	          json:{statements:[{statement:query,parameters:params}]}},
+	         function(err,res) { cb(err,res.body)})
+	}
+}
