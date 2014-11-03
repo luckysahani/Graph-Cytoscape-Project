e1147f6d04f739147651fdfd81e9579bee30d225
luckysahani <luckythegreat4@gmail.com>
2014-11-03 19:10:35 +0530

    trying out check function in parse

--- html/parse.js
+++ html/parse.js
@@ -5,11 +5,20 @@ function alert1(){
 	for (var i = 0; i < len; i++){
 		alert(split[i]);
 	}
-	// var r=require("request");
-	// var txUrl = "http://localhost:7474/db/data/transaction/commit";
-	// function cypher(query,params,cb) {
-	//   r.post({uri:txUrl,
-	//           json:{statements:[{statement:query,parameters:params}]}},
-	//          function(err,res) { cb(err,res.body)})
-	// }
+	console.log("alert1 clicked");
+	
+}
+function check()
+{
+	var neo4j = require("neo4j-js");
+	neo4j.connect('http://localhost:7474/db/data/', function (err, graph) {
+    if (err)
+        throw err;
+
+    else
+    	graph.createNode({ prop1: 'friends', prop2: 'maggu', boolProperty: false }, function (err, node) {
+		    console.log(err ? err : node);
+		});
+	});
+	console.log("check clicked");
 }
