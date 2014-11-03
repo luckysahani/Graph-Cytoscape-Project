1ab9186b784a1912b82fe6c8fd116d50c49ca451
root <root@lucky.complab.com>
2014-11-03 17:21:21 +0530

    added 1.html

--- html/parse.js
+++ html/parse.js
@@ -5,11 +5,11 @@ function alert1(){
 	for (var i = 0; i < len; i++){
 		alert(split[i]);
 	}
-	var r=require("request");
-	var txUrl = "http://localhost:7474/db/data/transaction/commit";
-	function cypher(query,params,cb) {
-	  r.post({uri:txUrl,
-	          json:{statements:[{statement:query,parameters:params}]}},
-	         function(err,res) { cb(err,res.body)})
-	}
+	// var r=require("request");
+	// var txUrl = "http://localhost:7474/db/data/transaction/commit";
+	// function cypher(query,params,cb) {
+	//   r.post({uri:txUrl,
+	//           json:{statements:[{statement:query,parameters:params}]}},
+	//          function(err,res) { cb(err,res.body)})
+	// }
 }
